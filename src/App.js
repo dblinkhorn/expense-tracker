bf543f17c8e653cae135e0bd5a121b5964bdca82
dblinkhorn <dblinkhorn@gmail.com>
2021-11-18 21:26:11 -0800

    finished adding dynamic adding of expense items from form input and dynamic displaying of each expense item from expenses array

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
 import Expenses from './components//Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import { useState } from 'react';
+
+const exampleExpenses = [
+  // {
+  //   id: 'expense1',
+  //   title: 'Toilet Paper',
+  //   amount: 94.12,
+  //   date: new Date(2020, 7, 14),
+  // },
+  // {
+  //   id: 'expense2',
+  //   title: 'New TV',
+  //   amount: 799.49,
+  //   date: new Date(2021, 2, 12)
+  // },
+  // {
+  //   id: 'expense3',
+  //   title: 'Car Insurance',
+  //   amount: 294.67,
+  //   date: new Date(2021, 2, 28),
+  // },
+  // {
+  //   id: 'expense4',
+  //   title: 'New Desk (Wooden)',
+  //   amount: 450,
+  //   date: new Date(2021, 5, 12),
+  // },
+];
 
 const App = () => {
-  // main data used as props in child components
-  const expenses = [
-    {
-      id: 'expense1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'expense2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'expense3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'expense4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  // set initial state to abive array of example expense objects
+  const [expenses, setExpenses] = useState(exampleExpenses);
 
   // function passed as a prop into NewExpense to retrieve
   // data from form input to be added to expenses array above
   const addExpenseHandler = (newExpense) => {
-    console.log('From App.js');
-    console.log(newExpense);
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
   };
 
   return (
     <div className="App">
       {/* pass function as custom prop into NewExpense */}
       <NewExpense addNewExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses expenseItem={expenses} />
     </div>
   );
 };
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,17 @@
-import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 // props inherited from App.js file
 const ExpenseItem = (props) => {
-
-  // destructure retun value of useState which always returns two items
-  // first, the current state value; second, the function which updates the state
-  const [title, setTitle] = useState(props.title);
-
-  // function to be used in button "onClick" property
-  const handleClick = () => {
-    setTitle('Updated!');
-  };
-
   return (
     <Card className='expense-item'>
       {/* props (props.date) inherited from App.js sent down again into ExpenseDate file */}
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={handleClick}>Edit Title</button>
     </Card>
   );
 };
